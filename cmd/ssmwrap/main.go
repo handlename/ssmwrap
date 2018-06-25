1ea5818d71dce7f242a8e32b780241658ab16c8b
handlename <nagata@handlena.me>
2018-06-25 11:25:58 +0900

    feat: add -v flag
    displays version

--- cmd/ssmwrap/main.go
+++ cmd/ssmwrap/main.go
@@ -9,15 +9,24 @@ import (
 	"github.com/handlename/ssmwrap"
 )
 
+var version string
+
 func main() {
 	var (
-		paths  string
-		prefix string
+		paths       string
+		prefix      string
+		versionFlag bool
 	)
 
 	flag.StringVar(&paths, "paths", "/", "comma separated parameter paths")
 	flag.StringVar(&prefix, "prefix", "", "prefix for environment variables")
+	flag.BoolVar(&versionFlag, "version", false, "display version")
 	flag.Parse()
+
+	if versionFlag {
+		fmt.Printf("ssmwrap v%s\n", version)
+		os.Exit(0)
+	}
 
 	if err := ssmwrap.Run(strings.Split(paths, ","), prefix, flag.Args()); err != nil {
 		fmt.Fprintln(os.Stderr, err)
