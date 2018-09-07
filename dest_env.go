51707b1adebe0412474dfbd7c41d1813a1ee32cb
handlename <nagata@handlena.me>
2018-09-07 10:53:37 +0900

    feat: destination implementation: env

---
+++ dest_env.go
@@ -0,0 +1,57 @@
+package ssmwrap
+
+import (
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/pkg/errors"
+)
+
+// DestinationSSM is implementation of Destination interface.
+type DestinationEnv struct {
+	Prefix string
+}
+
+func (d *DestinationEnv) Oputput(parameters map[string]string) error {
+	envVars := d.prepareEnvVars(parameters)
+	return d.export(envVars)
+}
+
+// prepareEnvironmentVariables transform SSM parameters to environment variables like `FOO=bar`
+// Tha last parts of parameter name separated by `/` will be used.
+// `prefix` will append to head of name of environment variables.
+func (d *DestinationEnv) prepareEnvVars(parameters map[string]string) []string {
+	envVars := d.formatParametersAsEnvVars(parameters)
+
+	// ssm parameters takes precedence over the current environment variables.
+	// In otehr words, ssm parameters overwrite the current environment variables.
+	envVars = append(os.Environ(), envVars...)
+
+	return envVars
+}
+
+func (d *DestinationEnv) formatParametersAsEnvVars(parameters map[string]string) []string {
+	envVars := []string{}
+
+	for name, value := range parameters {
+		parts := strings.Split(name, "/")
+		key := strings.ToUpper(d.Prefix + parts[len(parts)-1])
+		envVars = append(envVars, fmt.Sprintf("%s=%s", key, value))
+	}
+
+	return envVars
+}
+
+func (d *DestinationEnv) export(envVars []string) error {
+	for _, v := range envVars {
+		parts := strings.SplitN(v, "=", 2)
+		if len(parts) != 2 {
+			return errors.New("= is not contained in envvars")
+		}
+		if err := os.Setenv(parts[0], parts[1]); err != nil {
+			return errors.Wrap(err, "setenv failed")
+		}
+	}
+	return nil
+}
---
+++ dest_env_test.go
@@ -0,0 +1,103 @@
+package ssmwrap
+
+import (
+	"os"
+	"reflect"
+	"testing"
+)
+
+func TestFormatParametersAsEnvVars(t *testing.T) {
+	patterns := []struct {
+		Title           string
+		InputParameters map[string]string
+		InputPrefix     string
+		Expected        []string
+	}{
+		{
+			Title: "to upper",
+			InputParameters: map[string]string{
+				"/foo": "bar",
+			},
+			Expected: []string{
+				"FOO=bar",
+			},
+		},
+		{
+			Title: "deep path",
+			InputParameters: map[string]string{
+				"/d/e/e/p/path": "deep",
+			},
+			Expected: []string{
+				"PATH=deep",
+			},
+		},
+		{
+			Title: "add prefix",
+			InputParameters: map[string]string{
+				"/common/name": "john",
+			},
+			InputPrefix: "MY_",
+			Expected: []string{
+				"MY_NAME=john",
+			},
+		},
+		{
+			Title: "prefix will be upper, too",
+			InputParameters: map[string]string{
+				"/common/title": "event",
+			},
+			InputPrefix: "my_",
+			Expected: []string{
+				"MY_TITLE=event",
+			},
+		},
+	}
+
+	for _, pattern := range patterns {
+		t.Log(pattern.Title)
+
+		dest := DestinationEnv{
+			Prefix: pattern.InputPrefix,
+		}
+
+		envVars := dest.formatParametersAsEnvVars(pattern.InputParameters)
+
+		if !reflect.DeepEqual(envVars, pattern.Expected) {
+			t.Errorf("unexpected envVars: %+v", envVars)
+		}
+	}
+}
+
+func TestExport(t *testing.T) {
+	patterns := []struct {
+		Title    string
+		EnvVars  []string
+		Expected map[string]string
+	}{
+		{
+			Title:   "simple",
+			EnvVars: []string{"FOO=foo", "BAR=bar=baz"},
+			Expected: map[string]string{
+				"FOO": "foo",
+				"BAR": "bar=baz",
+			},
+		},
+	}
+
+	dest := DestinationEnv{}
+
+	for _, pattern := range patterns {
+		t.Log(pattern.Title)
+
+		err := dest.export(pattern.EnvVars)
+		if err != nil {
+			t.Error(err)
+		}
+
+		for key, value := range pattern.Expected {
+			if env := os.Getenv(key); env != value {
+				t.Errorf("unexpected env %s=%s (expected %s)", key, env, value)
+			}
+		}
+	}
+}
